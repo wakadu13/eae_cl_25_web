b7f592e86a2e9418fcf3afd7e13e57c987f05f6d
wakadu13 <antoine.wacogne@ecole-air.fr>
2025-10-28 11:56:55 +0100

    Refactor main function to calculate and print circle area based on radius

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def main():
-    n = 0
-    print("Hello from eae-cl-25-web!")
-    for i in range(1000000000):
-        n += i
-        print(n)
-        print("Hello world")
-#Mise au point
-if __name__ == "__main__":
-    main()
+pi = 3.14159
+rayon = 2.2
+# Calcul de l'aire du cercle
+aire = pi *(rayon**2)
+print(aire)
+#changer les valeurs du rayon
+#utiliser les commentaires pour aider les autres a comprendre ce code
+
+rayon += 1
+print(aire) #l'aire ne change pas
+aire = pi*(rayon**2)
+print(aire)
